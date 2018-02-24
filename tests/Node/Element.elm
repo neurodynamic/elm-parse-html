dca5e24bd05d7390c9e5d718852716f97de90032
neurodynamic <>
2018-02-23 21:48:35 -0800

    adds element tests

--- tests/Node/Element.elm
+++ tests/Node/Element.elm
@@ -12,14 +12,54 @@ suite : Test
 suite =
     describe "The Element module"
         [ describe "Element.element"
-            [ describe "self-closing tags"
-                [ test "Matches self-closing tags."
+            [ describe "element with opening and closing tags"
+                [ test "Matches basic element."
                     <| \_ ->
                         let
                             goodElement =
-                                "<input />"
+                                "<p></p>"
                         in
-                            Expect.equal (run element goodElement) (Ok (Element "input" [] []))
+                            Expect.equal (run element goodElement)
+                                (Ok (Element "p" [] []))
+                , test "Matches element attributes."
+                    <| \_ ->
+                        let
+                            goodElement =
+                                "<p class=\"some-class\"></p>"
+                        in
+                            Expect.equal (run element goodElement)
+                                (Ok (Element "p" [ ( "class", Just "some-class" ) ] []))
+                , test "Matches element attributes with no string."
+                    <| \_ ->
+                        let
+                            goodElement =
+                                "<button disabled></button>"
+                        in
+                            Expect.equal (run element goodElement)
+                                (Ok (Element "button" [ ( "disabled", Nothing ) ] []))
+                , test "Fails without closing tag."
+                    <| \_ ->
+                        let
+                            badElement =
+                                "<p>"
+                        in
+                            Expect.equal (run element badElement)
+                                (Err
+                                    { row = 1
+                                    , col = 4
+                                    , source = badElement
+                                    , problem = ExpectingSymbol "</p>"
+                                    , context = [ { row = 1, col = 1, description = "html element" } ]
+                                    }
+                                )
                 ]
+            , test "Matches self-closing tags."
+                <| \_ ->
+                    let
+                        goodElement =
+                            "<input type=\"text\" />"
+                    in
+                        Expect.equal (run element goodElement)
+                            (Ok (Element "input" [ ( "type", Just "text" ) ] []))
             ]
         ]
