71494f8847efeaf2e2d4cc5e05a181150317026f
M <stewartm@protonmail.com>
2018-02-24 11:21:33 -0800

    working-ish but commiting to new opening tag before trying closing tag

--- tests/TestHelp.elm
+++ tests/TestHelp.elm
@@ -9,7 +9,11 @@ expectProblem : Result Parser.Error Node -> Parser.Problem -> Expect.Expectation
 expectProblem result expectedProblem =
     case result of
         Err error ->
-            Expect.equal error.problem expectedProblem
+            let
+                errorThing =
+                    Debug.log "\n\nerror: " error
+            in
+                Expect.equal error.problem expectedProblem
 
         _ ->
             Expect.fail "Result should have been an error but wasn't."
