19ff7d1d36e5f355c1f62eb86d87946ee357fed5
neurodynamic <>
2018-02-26 09:19:28 -0800

    adds more details docs comments

--- ParseHtml.elm
+++ ParseHtml.elm
@@ -6,9 +6,12 @@ import ParseHtml.Node.Element exposing (element)
 import ParseHtml.Utils exposing (optionalSpaces)
 
 
+{-| This library is for parsing HTML documents. It uses the [elm-tools/parser](http://package.elm-lang.org/packages/elm-tools/parser/2.0.1) library to produce parsing results or appropriate types of errors, so you'll probably want to be familiar with that library as well to use this one. ParseHtml.Node.Model has the type you'll get from a successful parse.
+-}
 {-| Parser for an HTML document. Requires a DOCTYPE declaration and at least one HTML element to succeed.
 
-    document "<!DOCTYPE html><html></html>" == Ok (Element "html" [] [])
+    Parser.run document "<!DOCTYPE html><html></html>" == Ok (Element "html" [] [])
+    Parser.run document "<!DOCTYPE html><html><head></head><body><h1>I am a document!</h1></body></html>" = Element "html" [] [ Element "head" [] [], Element "body" [] [ Element "h1" [] [ TextNode "I am a document!" ]] ]
 -}
 document : Parser Node
 document =
@@ -21,6 +24,8 @@ document =
 
 
 {-| Runs the document parser.
+
+    parseDocument "<!DOCTYPE html><html></html>" == Ok (Element "html" [] [])
 -}
 parseDocument : String -> Result Error Node
 parseDocument html =
--- ParseHtml/Node/Model.elm
+++ ParseHtml/Node/Model.elm
@@ -11,6 +11,10 @@ type alias Content =
     String
 
 
+{-| Represents an HTML node. In most common use, will probably represent a complete tree of HTML elements parsed from an HTML document.
+
+    rootNode = Element "html" [] [ Element "head" [] [], Element "body" [] [ Element "h1" [] [ TextNode "I am a document!" ]] ]
+-}
 type Node
     = Element Name (List Attribute) (List Node)
     | TextNode Content
