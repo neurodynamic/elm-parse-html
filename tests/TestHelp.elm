bb02c6edd91d9646f1e0060487f09ef190c67056
neurodynamic <>
2018-02-26 07:52:49 -0800

    resolve TextNode test issues

--- tests/TestHelp.elm
+++ tests/TestHelp.elm
@@ -9,11 +9,7 @@ expectProblem : Result Parser.Error Node -> Parser.Problem -> Expect.Expectation
 expectProblem result expectedProblem =
     case result of
         Err error ->
-            let
-                errorThing =
-                    Debug.log "\n\nerror: " error
-            in
-                Expect.equal error.problem expectedProblem
+            Expect.equal error.problem expectedProblem
 
         _ ->
             Expect.fail "Result should have been an error but wasn't."
